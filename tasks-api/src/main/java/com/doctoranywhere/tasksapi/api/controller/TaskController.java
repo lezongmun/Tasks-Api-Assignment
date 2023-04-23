ae8f3f0f925e02aa6f2581288e269108842d987b
lezong <lezongmun@gmail.com>
2023-04-23 16:17:26 +0800

    Updated TaskController with getTask

--- tasks-api/src/main/java/com/doctoranywhere/tasksapi/api/controller/TaskController.java
+++ tasks-api/src/main/java/com/doctoranywhere/tasksapi/api/controller/TaskController.java
@@ -1,8 +1,25 @@
 package com.doctoranywhere.tasksapi.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.doctoranywhere.tasksapi.api.model.Task;
+import com.doctoranywhere.tasksapi.service.TaskService;
 
 @RestController
 public class TaskController {
 
+    private TaskService taskService;
+
+    @Autowired // auto injection of an instance of TaskService into constructor
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public Task getTask(@RequestParam Long id) {
+        return taskService.getTask(id);
+    }
 }
