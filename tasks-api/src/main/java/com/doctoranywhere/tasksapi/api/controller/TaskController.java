758ef745dfbd4fd6d6059d7224b537514fc68346
lezong <lezongmun@gmail.com>
2023-04-23 15:36:43 +0800

    Project structure created

---
+++ tasks-api/src/main/java/com/doctoranywhere/tasksapi/api/controller/TaskController.java
@@ -0,0 +1,8 @@
+package com.doctoranywhere.tasksapi.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+
+}
---
+++ tasks-api/src/main/java/com/doctoranywhere/tasksapi/api/model/Task.java
@@ -0,0 +1,48 @@
+package com.doctoranywhere.tasksapi.api.model;
+
+public class Task {
+    private Long id; // unique identifier for the task
+    private String title; // title of the task
+    private String description; // description of the task
+    private Boolean completed; // flag indicating whether the task has been completed
+
+    public Task(Long id, String title, String description, Boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setID(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+}
