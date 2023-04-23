db6cd5fc3ba82eec4ada7229ad38525cf9a49e2c
lezong <lezongmun@gmail.com>
2023-04-24 03:50:26 +0800

    Working delete task by id and update task

--- tasks-api/src/main/java/com/doctoranywhere/tasksapi/api/controller/TaskController.java
+++ tasks-api/src/main/java/com/doctoranywhere/tasksapi/api/controller/TaskController.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,30 +29,53 @@ public class TaskController {
 
     // Get a single task by ID
     @GetMapping("/tasks/{id}")
-    public Task getTask(@PathVariable Long id) {
+    public ResponseEntity<Task> getTask(@PathVariable Long id) {
         Optional<Task> task = taskService.getTask(id);
         if (task.isPresent()) {
-            return (Task) task.get();
+            return ResponseEntity.ok(task.get());
         }
-        return null;
-
+        return ResponseEntity.notFound().build();
     }
 
     // Get a list of all tasks
     @GetMapping("/tasks")
     public ArrayList<Task> getTasks() {
-        Optional<ArrayList<Task>> tasks = taskService.getTasks();
-        if (tasks.isPresent()) {
-            return (ArrayList<Task>) tasks.get();
-        }
-        return null;
+        ArrayList<Task> tasks = taskService.getTasks();
+        return tasks;
     }
 
+    // Create a new task
     @PostMapping("/tasks")
-    public Task createTask(@RequestParam Long id, @RequestParam String title, @RequestParam String description,
+    public ResponseEntity<Task> createTask(@RequestParam Long id, @RequestParam String title,
+            @RequestParam String description,
             @RequestParam Boolean completed) {
-        Task task = taskService.createTask(id, title, description, completed);
-        return task;
+        Optional<Task> task = taskService.createTask(id, title, description, completed);
+        if (task.isPresent()) {
+            return ResponseEntity.ok(task.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 
+    // Update a task by ID
+    @PutMapping("/tasks/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id,
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "completed", required = false) Boolean completed) {
+        Optional<Task> task = taskService.updateTask(id, title, description, completed);
+        if (task.isPresent()) {
+            return ResponseEntity.ok(task.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    // Delete a task by ID
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<Task> deleteTask(@PathVariable Long id) {
+        Optional<Task> task = taskService.deleteTask(id);
+        if (task.isPresent()) {
+            return ResponseEntity.ok(task.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
--- tasks-api/src/main/java/com/doctoranywhere/tasksapi/service/TaskService.java
+++ tasks-api/src/main/java/com/doctoranywhere/tasksapi/service/TaskService.java
@@ -1,6 +1,7 @@
 package com.doctoranywhere.tasksapi.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,39 +12,66 @@ import com.doctoranywhere.tasksapi.api.model.Task;
 @Service
 public class TaskService {
 
-    private ArrayList<Task> tasks;
+    private HashMap<Long, Task> tasks;
 
     public TaskService() {
-        tasks = new ArrayList<>();
+        tasks = new HashMap<>();
         Task task1 = new Task((long) 1, "title", "description", false);
-        tasks.add(task1);
+        tasks.put((long) 1, task1);
     }
 
     // Get a single task by ID
     public Optional<Task> getTask(Long id) {
         // Optional optionalTask = Optional.empty();
-        for (Task task : tasks) {
-            if (id == task.getId()) {
-                return Optional.of(task);
-            }
+        if (tasks.containsKey(id)) {
+            Task task = tasks.get(id);
+            return Optional.of(task);
         }
+
         // task not found
         return Optional.empty();
     }
 
-    public Optional<ArrayList<Task>> getTasks() {
+    public ArrayList<Task> getTasks() {
         // Optional<ArrayList<Task>> optional = Optional.empty();
-        if (tasks.size() == 0) {
-            return Optional.empty();
-        }
-        ArrayList<Task> tasksCopy = new ArrayList<>(tasks);
-        return Optional.of(tasksCopy);
+        ArrayList<Task> taskList = new ArrayList<>(tasks.values());
+        return taskList;
     }
 
-    public Task createTask(Long id, String title, String description, Boolean completed) {
+    public Optional<Task> createTask(Long id, String title, String description, Boolean completed) {
+        // if task id already exists in tasks
+        if (tasks.containsKey(id)) {
+            return Optional.empty();
+        }
         Task newTask = new Task(id, title, description, completed);
-        tasks.add(newTask);
-        return newTask;
+        tasks.put(id, newTask);
+        return Optional.of(newTask);
+    }
+
+    public Optional<Task> updateTask(Long id, String title, String description, Boolean completed) {
+        if (tasks.containsKey(id)) {
+            if (title != null) {
+                tasks.get(id).setTitle(title);
+            }
+            if (description != null) {
+                tasks.get(id).setDescription(description);
+            }
+            if (completed != null) {
+                tasks.get(id).setCompleted(completed);
+            }
+            return Optional.of(tasks.get(id));
+        }
+        return Optional.empty(); // task not found
+    }
+
+    public Optional<Task> deleteTask(Long id) {
+        if (tasks.containsKey(id)) {
+            Optional<Task> optional = Optional.of(tasks.get(id));
+            tasks.remove(id);
+            return optional;
+        }
+        // task not found
+        return Optional.empty();
     }
 
 }
